248f825213161b5effa914fde64b890c6e98b4d9
alina <zmalina98@gmail.com>
2024-02-11 21:56:33 +0600

    change in card

--- src/roles/Student/ProductCardTemplate.jsx
+++ src/roles/Student/ProductCardTemplate.jsx
@@ -9,16 +9,16 @@
     <div className='grid xl:grid-cols-3 md:grid-cols-4 justify-between w-full p-5 border text-left md:gap-4 lg:gap-5 xl:gap-7'>
 
 
-        <div className="xl:col-span-1 overflow-hidden md:col-span-2 bg-myCard border-2 border-gray-300 rounded-2xl shadow-xl dark:bg-gray-800 dark:border-gray-700">
-
-            <img className="transition hover:scale-105 duration-200 hover:shadow-lg" src="https://cdn.sparkfun.com/assets/learn_tutorials/4/7/12615-02_Full_Size_Breadboard_Split_Power_Rails.jpg" alt="" />
-
-            <div className="p-5">
+        <div className="xl:col-span-1 overflow-hidden md:col-span-2 h-[450px] bg-myCard border-2 border-gray-300 rounded-2xl shadow-xl dark:bg-gray-800 dark:border-gray-700">
+            <div className='h-[250px] bg-white overflow-hidden flex items-center justify-center'>
+                <img className="transition hover:scale-105 duration-200 hover:shadow-lg " src="https://www.robotechbd.com/wp-content/uploads/2021/07/frosted-leds-red-green-blue-yellow-white-800x800-1.jpg" alt="" />
+            </div>
+            <div className="p-5 relative">
 
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-myText dark:text-white">Breadboard</h5>
 
                 <p className="mb-3 text-base text-gray-500 text-pretty ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                <Link to={''} onClick={() => { }} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-primary rounded-lg hover:shadow-xl hover:scale-105 active:scale-95 transition duration-100">
+                <Link to={''} onClick={() => { }} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-primary rounded-lg hover:shadow-xl hover:scale-105 active:scale-95 transition duration-100 absolute -bottom-5">
                     Details
                     <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
@@ -28,6 +28,7 @@
         </div>
 
     </div>
+
 
 
 
